2ca045d268bb64ff79c61549444c26f9d712a7c2
Tofik Abdu <104857366+tans1@users.noreply.github.com>
2022-12-23 05:21:04 -0900

    Time: 85 ms (13.51%), Space: 13.9 MB (38.77%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -1,27 +1,12 @@
+import heapq
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        if not nums1: return nums2
-        if not nums2: return nums1
-        
-        for i in range(n):
-            nums1[m] = nums2[i]
-            m += 1
-        nums1.sort()
-        return
-    ########################################
-        i = m - 1
-        j = n - 1
-        k = m + n - 1
-        while i >= 0 and j >= 0:
-            if nums1[i] >= nums2[j]:
-                nums1[k], nums1[i] = nums1[i], nums1[k]
-                k -= 1
-                i -= 1
-            else:
-                nums1[k] = nums2[j]
-                j -= 1
-                k -= 1
+        temp = nums1.copy()
+        temp2 = list(heapq.merge(temp[:m],nums2))
+        for i in range(len(temp2)):
+            nums1[i] = temp2[i]
         print(nums1)
+        
