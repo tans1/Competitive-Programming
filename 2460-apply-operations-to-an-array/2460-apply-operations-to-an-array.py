3961767a8265ae4a9f765204aa46e825daaa61e3
Tofik Abdu <104857366+tans1@users.noreply.github.com>
2023-01-12 05:19:15 -0900

    Time: 44 ms (99.14%), Space: 14.1 MB (95.90%) - LeetHub

--- 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.py
+++ 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.py
@@ -6,9 +6,8 @@ class Solution:
                 nums[i] = 0
         res = []
         for n in nums:
-            if n != 0:
-                res.append(n)
+            if n != 0: res.append(n)
+                
         x = len(nums) - len(res)
-        
         res.extend([0]*x)
         return res
