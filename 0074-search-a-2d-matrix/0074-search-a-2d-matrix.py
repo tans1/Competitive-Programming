460cc09d8abcbced3d6951a4285d86d1d67451aa
Tofik Abdu <104857366+tans1@users.noreply.github.com>
2023-01-12 07:22:14 -0900

    Time: 49 ms (71.81%), Space: 14.3 MB (99.56%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -8,18 +8,3 @@ class Solution:
             if nm == target:
                 return True
         return False
-        
-        
-        
-        
-        
-        
-        # i = 0
-        # cl = len(matrix[0])-1
-        # while i < len(matrix) and target not in matrix[i]:
-        #     i += 1
-        # if i < len(matrix) and target in matrix[i]:
-        #     return True
-        # return False
-        
-            
