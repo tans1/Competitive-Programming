9c67615c2acba52796083ce0c38d934e5db0c0d9
Tofik Abdu <104857366+tans1@users.noreply.github.com>
2023-01-17 07:59:46 -0900

    Time: 539 ms (11.63%), Space: 15.7 MB (8.95%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -1,5 +1,22 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
+        temp = [0 for _ in range(max(heights) + 1)]
+        dct = {}
+
+        for i in range(len(heights)):
+            temp[heights[i]] += 1
+            dct[heights[i]] = names[i]
+            
+        res = []
+        for i in range(len(temp)):
+            if temp[i] != 0 and i in dct:
+                res.extend([dct[i]] * temp[i])
+        res.reverse()
+        return res
+#         indexes = [i for i in range(len(heights))]
+#         indexes.sort(key=lambda x: heights[x], )
+#         names = [names[i] for i in indexes]
+        
         
 #         # names.sort(key= lambda i:)
 #         # idx = list(range(len(heights)))
@@ -39,21 +56,25 @@ class Solution:
 #         return names
     
     
-        #insertion sort
-        if len(names) < 2: return names
+#         #insertion sort
+#         if len(names) < 2: return names
         
 
-        for i in range(1, len(heights)):
-            j = i
-            while j >= 1 and heights[j] >= heights[j-1] :
+#         for i in range(1, len(heights)):
+#             j = i
+#             while j >= 1 and heights[j] >= heights[j-1] :
                 
-                heights[j], heights[j-1] = heights[j-1], heights[j]
-                names[j], names[j-1] = names[j-1], names[j]
+#                 heights[j], heights[j-1] = heights[j-1], heights[j]
+#                 names[j], names[j-1] = names[j-1], names[j]
                 
-                j -= 1
+#                 j -= 1
 
-        # names.reverse()
-        return names
+#         return names
+
+#sort by counting
+
+    
+    
                 
         
         
