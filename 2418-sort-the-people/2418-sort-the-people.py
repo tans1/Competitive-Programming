21691af3cda56080ba04fef3f2f6c3b0ae13371b
Tofik Abdu <104857366+tans1@users.noreply.github.com>
2023-01-17 07:08:21 -0900

    Time: 1058 ms (7.74%), Space: 14.4 MB (84.60%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -1,13 +1,34 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        # return sorted(names , key= lambda x: heights.index)
-        for i in range(len(heights)):
-            for  j in range(len(heights)-1):
-                if heights[j] > heights[j+1]:
-                    
-                    heights[j], heights[j+1] = heights[j+1], heights[j]
-                    names[j], names[j+1] = names[j+1], names[j]
-        
+        i = 0
+        while i < len(heights)-1:
+            
+            j = i
+            min_index = j
+            
+            while j < len(heights):
+                if heights[j] < heights[min_index]:
+                    min_index = j
+                j += 1
+
+            heights[i], heights[min_index] = heights[min_index], heights[i]
+            names[i], names[min_index] = names[min_index], names[i]
+            
+            i += 1
+
         names.reverse()
+        
         return names
+            # return sorted(names , key= lambda x: heights.index)
+#         for i in range(len(heights)):
+#             for  j in range(len(heights)-1):
+#                 if heights[j] > heights[j+1]:
+                    
+#                     heights[j], heights[j+1] = heights[j+1], heights[j]
+#                     names[j], names[j+1] = names[j+1], names[j]
+        
+#         names.reverse()
+        # return names
+    
+    
         
