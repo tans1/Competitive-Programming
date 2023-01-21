b53fffd895cea7fe9603dba2b0eca1c450f9ca31
Tofik Abdu <104857366+tans1@users.noreply.github.com>
2023-01-21 13:59:44 -0900

    Time: 592 ms (27.86%), Space: 15.9 MB (13.82%) - LeetHub

---
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.py
@@ -0,0 +1,17 @@
+class Solution:
+    def judgeSquareSum(self, c: int) -> bool:
+        if c == 1 or c == 0 : return True
+        ls = [i for i in range( int(c ** 0.5) + 1)]
+        i = 0
+        j = len(ls)-1
+        
+        while i <= j:
+            if (ls[i] ** 2) + (ls[j] ** 2) == c:
+                print(ls[i], ls[j])
+                return True
+            else:
+                if (ls[i] ** 2) + (ls[j] ** 2) < c:
+                    i += 1
+                else:
+                    j -= 1
+        return False
