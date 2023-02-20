9ff153a2c69944fc836b4c949941967b41638fd4
Tofik Abdu <104857366+tans1@users.noreply.github.com>
2023-02-20 05:28:22 -0900

    Time: 65 ms (23.36%), Space: 17.4 MB (55.24%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.py
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.py
@@ -7,23 +7,26 @@
 class Solution:
     def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
         if not head or not head.next: return 
-        dct = defaultdict(int)
-        node = head
-        while node:
-            if node in dct:
-                return dct[node]
-            dct[node] = node
-            node = node.next
-        return 
+        # dct = defaultdict(int)
+        # node = head
+        # while node:
+        #     if node in dct:
+        #         return dct[node]
+        #     dct[node] = node
+        #     node = node.next
+        # return 
     
-#         slow = head
-#         fast = head
-        
-#         while fast and fast.next:
-#             if fast != head and fast == slow:
-#                 break
-#             fast = fast.next.next
-#             slow = slow.next
-#         if fast == slow:
+        fast = slow =head
+        while fast and fast.next and slow:
+            fast = fast.next.next
+            slow = slow.next
+            if slow == fast:
+                node = head
+                while node != fast:
+                    node = node.next
+                    fast = fast.next
+                return fast
+        return 
+
             
             
