58516ac8449e9e9a42827ef38f2504aee0eef727
Tofik Abdu <104857366+tans1@users.noreply.github.com>
2023-02-25 10:15:05 -0900

    Time: 386 ms (23.95%), Space: 15.3 MB (26.05%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -8,36 +8,10 @@ class Solution:
             if s[i+n-1] not in sp:
                 sp[s[i+n-1]] = 0
             sp[s[i+n-1]] += 1
+            
             if sp == cp:
                 res.append(i)
             sp[s[i]] -= 1
             if sp[s[i]] == 0:
                 del sp[s[i]]
         return res
-#         res = []
-#         fr = Counter(p)
-#         size = len(p)
-#         windowCount = Counter(s[:size])
-        
-#         if windowCount == fr:
-#             res.append(0)
-        
-#         for i in range(size, len(s)):
-#             windowCount[s[i-size]] -= 1
-#             if not windowCount[s[i-size]]:
-#                 windowCount.pop(s[i-size])
-                
-#             if s[i] in windowCount:
-#                 windowCount[s[i]] += 1
-                
-#             else:
-#                 windowCount[s[i]] = 1
-                
-            
-#             if windowCount == fr:
-#                 res.append(i-size+1)
-                
-                
-#         return res
-        
-                
