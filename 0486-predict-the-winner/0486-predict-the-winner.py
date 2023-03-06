244d4c5f8397409fbec32a76c6658a903de5cb64
Tofik Abdu <104857366+tans1@users.noreply.github.com>
2023-03-05 21:19:36 -0900

    Time: 37 ms (75.55%), Space: 14.1 MB (23.20%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.py
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -1,16 +1,23 @@
 class Solution:
     def PredictTheWinner(self, nums: List[int]) -> bool:
+        stack_memo = {}
         
         def heleper(l,r,turn):
             if l > r :
                 return 0
-            
-            if turn:
-                return max(nums[l] + heleper(l+1, r, not turn),  nums[r] + heleper(l, r-1, not turn))
-            
             else:
-                return min(-nums[l] + heleper(l+1, r, not turn),-nums[r] + heleper(l, r-1, not turn))
-        
+                if (l,r,turn) in stack_memo:
+                    return stack_memo[(l,r,turn)]
+                else:
+                    
+                    if turn:
+                        stack_memo[(l,r,turn)] = max(nums[l] + heleper(l+1, r, not turn),  nums[r] + heleper(l, r-1, not turn))
+
+                    else:
+                        stack_memo[(l,r,turn)] = min(-nums[l] + heleper(l+1, r, not turn),-nums[r] + heleper(l, r-1, not turn))
+                    
+                    return stack_memo[(l,r,turn)]
+                
         return heleper(0,len(nums)-1,True) >= 0
             
                 
