da479c6e8c1e5e799c7d48278a8de387da8a2017
Tofik Abdu <104857366+tans1@users.noreply.github.com>
2023-03-09 02:18:24 -0900

    Time: 1623 ms (5.04%), Space: 16.2 MB (7.67%) - LeetHub

---
+++ 0077-combinations/0077-combinations.py
@@ -0,0 +1,27 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        ans = []
+        
+        def helper(cur,stack):
+            if len(stack) == k:
+                newList = stack.copy()
+                if len(set(newList)) == k:
+                    ans.append(newList)
+                stack.pop()
+                return
+            current = [x for x in range(cur+1,n+1)]
+            
+            for num in current:
+                s = stack.copy()
+                s.append(num)
+                helper(num,s)
+        
+        for i in range(n):
+            helper(i,[])
+        
+        res = []
+        
+        for a in ans:
+            if a not in res:
+                res.append(a)
+        return res
