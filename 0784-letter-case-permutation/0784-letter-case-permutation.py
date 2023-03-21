e35ca324f69c72089cf326d842cb887a4a549321
Tofik Abdu <104857366+tans1@users.noreply.github.com>
2023-03-21 19:54:12 +0300

    Time: 91 ms (14.36%), Space: 17.8 MB (5.44%) - LeetHub

---
+++ 0784-letter-case-permutation/0784-letter-case-permutation.py
@@ -0,0 +1,28 @@
+class Solution:
+    def letterCasePermutation(self, s: str) -> List[str]:
+        res = set()
+        
+        def dfs(S,i):
+            if i >= len(s):
+                res.add(tuple(S))
+                return
+            
+            
+            S[i] = S[i].upper()
+            dfs(S,i+1)
+            
+            S[i] = S[i].lower()
+            dfs(S,i + 1)
+            
+        
+        S = list(s)
+        
+        dfs(S,0)
+        
+        ans = list(map(lambda x : ''.join(list(x)), res))
+
+        return ans
+            
+            
+            
+            
