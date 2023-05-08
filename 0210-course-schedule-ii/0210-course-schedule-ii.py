e5681f49435d427ac1eca86a20b0611eda3041bf
Tofik Abdu <104857366+tans1@users.noreply.github.com>
2023-05-08 19:06:43 +0300

    Time: 109 ms (33.59%), Space: 19.4 MB (8.28%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,28 +1,87 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        color = [0 for _ in range(numCourses)]
         graph = defaultdict(list)
-        incoming = [0] * numCourses
         
-        for a,b in prerequisites:
+        for a, b in prerequisites:
             graph[b].append(a)
-            incoming[a] += 1
         
-        todo = deque([])
-        
-        for i , count in enumerate(incoming):
-            if count == 0:
-                todo.append(i)
-        
+        self.cycle = False
         ans = []
-        while todo:
-            nd = todo.popleft()
-            ans.append(nd)
+        def dfs(nd): 
+            color[nd] = 1
+            
             for nb in graph[nd]:
-                incoming[nb] -= 1
+                if color[nb] == 1:
+                    self.cycle = True
+                    return 
                 
-                if incoming[nb] == 0:
-                    todo.append(nb)
+                if color[nb] == 0:
+                    dfs(nb)
+            ans.append(nd)
+            color[nd] = 2
         
-        if len(ans) != numCourses:
+        for i in range(len(color)):
+            if color[i] == 0:
+                dfs(i)
+                
+        if len(ans) != numCourses or self.cycle == True:
             return []
-        return ans
+        return ans[::-1]
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         graph = defaultdict(list)
+#         incoming = [0] * numCourses
+        
+#         for a,b in prerequisites:
+#             graph[b].append(a)
+#             incoming[a] += 1
+        
+#         todo = deque([])
+        
+#         for i , count in enumerate(incoming):
+#             if count == 0:
+#                 todo.append(i)
+        
+#         ans = []
+#         while todo:
+#             nd = todo.popleft()
+#             ans.append(nd)
+#             for nb in graph[nd]:
+#                 incoming[nb] -= 1
+                
+#                 if incoming[nb] == 0:
+#                     todo.append(nb)
+        
+#         if len(ans) != numCourses:
+#             return []
+#         return ans
