73cd911e4d31fd9024b006c7248753870d98ef20
Tofik Abdu <104857366+tans1@users.noreply.github.com>
2023-05-17 18:18:22 +0300

    Time: 1926 ms (66.06%), Space: 107.3 MB (58.38%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
@@ -1,25 +1,98 @@
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
-        if len(edges) <= 1:
-            return True
-        graph = defaultdict(list)
+        parent = [i for i in range(n)]
+        rank = [1 for _ in range(n)]
+        
+        def find(x):
+            nonlocal parent
+            root = x
+            while root != parent[root]:
+                root = parent[root]
+
+            while x != root:
+                par = parent[x]
+                parent[x] = root
+                x = par
+
+            return root
+
+
+
+
+            # if x != parent[x]:
+            #     temp = find(parent[x])
+            #     parent[x] = temp 
+            #     return temp
+            # return x
+        
+        
+        def union(x,y):
+            par1,par2 = find(x), find(y)
+            
+            if par1 == par2:
+                return 
+            
+            if rank[par1] == rank[par2]:
+                # parent[par2] = par1
+                rank[par1] += 1
+                
+
+            if rank[par1] < rank[par2]:
+                parent[par1] = par2
+                
+            else:
+                parent[par2] = par1
+                            
+        
         for u,v in edges:
-            graph[u].append(v)
-            graph[v].append(u)
+            union(u,v)
         
 
-        stack = [source]
-        visited = set()
-        
-        while stack:
-            nd = stack.pop()
-            visited.add(nd)
+        return find(source) == find(destination)
             
-            for neighbours in graph[nd]:
-                if neighbours == destination:
-                    return True
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         if len(edges) <= 1:
+#             return True
+#         graph = defaultdict(list)
+#         for u,v in edges:
+#             graph[u].append(v)
+#             graph[v].append(u)
+        
+
+#         stack = [source]
+#         visited = set()
+        
+#         while stack:
+#             nd = stack.pop()
+#             visited.add(nd)
+            
+#             for neighbours in graph[nd]:
+#                 if neighbours == destination:
+#                     return True
                 
-                if neighbours not in visited:
-                    stack.append(neighbours)
+#                 if neighbours not in visited:
+#                     stack.append(neighbours)
                     
-        return False
+#         return False
