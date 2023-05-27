5b4688fb770b4828bd05784a7fd9bdf0278c3e18
Tofik Abdu <104857366+tans1@users.noreply.github.com>
2023-05-27 22:33:40 +0300

    Time: 543 ms (45.58%), Space: 17.7 MB (19.73%) - LeetHub

--- 0659-split-array-into-consecutive-subsequences/0659-split-array-into-consecutive-subsequences.py
+++ 0659-split-array-into-consecutive-subsequences/0659-split-array-into-consecutive-subsequences.py
@@ -1,23 +1,23 @@
 class Solution:
     def isPossible(self, nums: List[int]) -> bool:
         frequncy = Counter(nums)
-        needed = defaultdict(int)
+        can_join = defaultdict(int)
         
         for n in nums:
             if frequncy[n] == 0:
                 continue
             
-            if needed[n] > 0:
-                needed[n] -= 1
-                needed[n+1] += 1
+            if can_join[n] > 0:
+                can_join[n] -= 1
+                can_join[n+1] += 1
                 
             
             elif frequncy[n+1] > 0 and frequncy[n + 2] > 0:
                 frequncy[n+1] -= 1
                 frequncy[n+2] -= 1
-                needed[n+3] += 1
+                can_join[n+3] += 1
             
-            elif needed[n] == 0 or frequncy[n+1] <= 0 or frequncy[n+2] <= 0:
+            elif can_join[n] == 0 or frequncy[n+1] <= 0 or frequncy[n+2] <= 0:
                 return False
             
             frequncy[n] -= 1
