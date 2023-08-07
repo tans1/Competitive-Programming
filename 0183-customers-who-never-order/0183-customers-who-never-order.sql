fcd8d5ff2bbec93bc2e0f9ce5e185854937f8c48
Tofik Abdu <104857366+tans1@users.noreply.github.com>
2023-08-07 14:25:08 +0300

    Time: 1406 ms (23.14%), Space: 0B (100.00%) - LeetHub

--- 0183-customers-who-never-order/0183-customers-who-never-order.sql
+++ 0183-customers-who-never-order/0183-customers-who-never-order.sql
@@ -1,16 +1,16 @@
 
-# select name as 'Customers'
-# from Customers
-# left join Orders 
-# on Customers.id = Orders.customerId
-# where Orders.customerId is null
+select name as 'Customers'
+from Customers
+left join Orders 
+on Customers.id = Orders.customerId
+where Orders.customerId is null
 
 # or 
 
-select name as 'Customers'
-from Customers
-where id not in 
-(
-    select customerId
-    from Orders
-)
+# select name as 'Customers'
+# from Customers
+# where id not in 
+# (
+#     select customerId
+#     from Orders
+# )
