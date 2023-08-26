a14b08028984c3a87f47bb99d12f8d3d6db1fd15
Tofik Abdu <104857366+tofik-abdu-a2sv@users.noreply.github.com>
2023-08-26 22:22:13 +0300

    Time: 4219 ms (14.50%), Space: 16.2 MB (73.60%) - LeetHub

---
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.py
@@ -0,0 +1,15 @@
+class Solution:
+    def countVowelStrings(self, n: int) -> int:
+        vowels = ['a','e','i','o','u']
+        ans = 0
+        
+        # @lru_cache
+        def dfs(i,cur):
+            nonlocal ans
+            if len(cur) == n:
+                ans += 1
+                return
+            for j in range(i,len(vowels)):
+                dfs(j,cur + vowels[j])
+        dfs(0,"")
+        return ans
