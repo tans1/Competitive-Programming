9f4ad96ebd07dac5f868b022550a2f6cba96de60
Tofik Abdu <104857366+tofik-abdu-a2sv@users.noreply.github.com>
2023-09-03 16:22:38 +0300

    Time: 38 ms (74.33%), Space: 16.4 MB (19.18%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -4,7 +4,7 @@ class Solution:
         dp[0][0] = 1
         for i in range(m):
             for j in range(n):
-                if i - 1 >= 0 :
+                if i - 1 >= 0 : 
                     dp[i][j] += dp[i-1][j]
                 if j-1 >= 0 :
                     dp[i][j] += dp[i][j-1]
