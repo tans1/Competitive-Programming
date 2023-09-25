dad039ff0abdec595929e64effd3959099a9844c
Tofik Abdu <104857366+tans1@users.noreply.github.com>
2023-09-25 16:05:44 +0300

    Time: 1469 ms (46.71%), Space: 16.8 MB (23.35%) - LeetHub

--- 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.py
+++ 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.py
@@ -1,6 +1,6 @@
 class Solution:
     def bestTeamScore(self, scores: List[int], ages: List[int]) -> int:
-        res = [0 for _ in range(len(scores))]
+        dp = [0 for _ in range(len(scores))]
         
         combined = []
         for i in range(len(scores)):
@@ -10,6 +10,6 @@ class Solution:
         for i in range(len(combined)-1,-1,-1):
             for j in range(i,len(combined)):
                 if combined[i][1] <= combined[j][1]:
-                    res[i] = max(res[i],res[j] + combined[i][1] )
-        return max(res)
+                    dp[i] = max(dp[i],dp[j] + combined[i][1] )
+        return max(dp)
             
