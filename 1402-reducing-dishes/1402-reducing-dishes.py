1c0103fcbeb864e36df77202c454ac7ce1a9017b
Tofik Abdu <104857366+tans1@users.noreply.github.com>
2023-10-06 10:45:57 +0300

    Time: 46 ms (73.61%), Space: 16.3 MB (81.12%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.py
+++ 1402-reducing-dishes/1402-reducing-dishes.py
@@ -1,17 +1,16 @@
 class Solution:
     def maxSatisfaction(self, satisfaction: List[int]) -> int:
-        def likeTimeCoefficient(arr):
-            result = 0
-            for i, v in enumerate(arr):
-                result += v*(i+1)
-            return result
-        
         satisfaction.sort()
-        dct = {}
-        for i in range(len(satisfaction)-1,-1,-1):
-            dct[i] = likeTimeCoefficient(satisfaction[i:])
+        suffix_sum = [0 for _ in range(len(satisfaction))]
+        for i in range(len(satisfaction)-2,-1,-1):
+            suffix_sum[i] = suffix_sum[i+1] + satisfaction[i+1]
         
-        ans = max(dct.values())
-        if ans >= 0:
+        temp = [0 for _ in range(len(satisfaction) + 1)]
+        for i in range(len(satisfaction)-1,-1,-1):
+            temp[i] = temp[i+1] + satisfaction[i] + suffix_sum[i]
+        ans = max(temp[:len(satisfaction)])
+
+        if ans > 0:
             return ans
         return 0
+        
