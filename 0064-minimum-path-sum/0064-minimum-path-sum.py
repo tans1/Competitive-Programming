2f30c4542c27329ca7d734eab7ce6a91a9f2c685
Tofik Abdu <104857366+tans1@users.noreply.github.com>
2023-12-31 15:15:37 +0300

    Time: 201 ms (5.01%), Space: 19 MB (21.75%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -1,53 +1,19 @@
 class Solution:
-    def minPathSum(self, grid: List[List[int]]) -> int:        
+    def minPathSum(self, grid: List[List[int]]) -> int:
         for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if i - 1 < 0 or j - 1 < 0:
-                    if i - 1 < 0 and j - 1 >= 0:
-                        grid[i][j] += grid[i][j-1]
-                        
-                    elif i - 1 >= 0 and j - 1 < 0:
-                        grid[i][j] += grid[i-1][j]
-                else:
-                    grid[i][j] += min(grid[i-1][j], grid[i][j-1])
-        
-        return grid[-1][-1]
-                
-        
-        
-        
-        
-        
-#         self.ans = float('inf')
+          for j in range(len(grid[0])):
+            if i == 0 and j == 0:
+              continue
+            
+            top = float('inf')
+            left = float('inf')
 
-        
-#         def backtracking(i,j,sm):
+            if 0 <= i - 1 < len(grid):
+              top = grid[i-1][j]
+            if 0 <= j - 1 < len(grid[0]):
+              left = grid[i][j-1]
             
-#             if i >= len(grid)-1 and j >= len(grid[0]):
-#                 self.ans = min(self.ans, sm)
-#                 return
-            
-#             if i >= len(grid) or j >= len(grid[0]):
-#                 return
-            
-            
-#             backtracking(i+1,j,sm + grid[i][j])
-#             backtracking(i,j+1,sm + grid[i][j])
-        
-        
-#         backtracking(0,0,0)
-        
-#         return self.ans
-            
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+            grid[i][j] = min(top,left) + grid[i][j]
+          
+
+        return grid[-1][-1]
